269f69ba66cce6ed8322b3fed134663e1714ea33
Felipe Silva <felipe.dacs@gmail.com>
2021-05-26 01:42:32 -0300

    :new: resolve desafio

---
+++ README.md
@@ -0,0 +1,5 @@
+# desafio
+
+- Instalar dependências ``yarn``
+- Rodar servidor/api ``yarn server``
+- Com outro terminal, iniciar aplicação ``yarn dev``
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,40 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface Props {
+  selectedGenre: string;
+  movies: MovieProps[];
+}
+
+export function Content(props: Props) {
+
+  return(
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.selectedGenre}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
